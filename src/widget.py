7857808bd1a0db0a3917848376744caa63293ae1
Роман Шумаков <romeo46100@mail.ru>
2026-04-06 11:51:42 +0300

    creating widget with mask_account_card & get_date

---
+++ src/widget.py
@@ -0,0 +1,25 @@
+from masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(card: str) -> str:
+    """Функция обработки информации как о картах, так и о счетах"""
+    splited_card = card.split()
+    words_list = []
+    for word in splited_card:
+        if word.isdigit() and len(word) == 16:
+            word = get_mask_card_number(int(word))
+        if word.isdigit() and len(word) == 20:
+            word = get_mask_account(int(word))
+
+        words_list.append(word)
+    masked_card = " ".join(words_list)
+    return masked_card
+
+
+def get_date(date: str) -> str:
+    """Функция изменения формата даты"""
+
+    inverted_date = date[:10].split("-")
+    formated_date = f"{inverted_date[2]}.{inverted_date[1]}.{inverted_date[0]}"
+    return formated_date
+
