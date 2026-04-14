dab04dc88f4f06f0e69831bf0a8249c58c244f9c
Роман Шумаков <romeo46100@mail.ru>
2026-04-15 01:37:54 +0300

    add function filter_by_state in processing.py,

---
+++ src/processing.py
@@ -0,0 +1,6 @@
+def filter_by_state(data_list: list[dict], state='EXECUTED') -> list[dict]:
+    result_list = []
+    for diction in data_list:
+        if diction["state"] == state:
+            result_list.append(diction)
+            return result_list
