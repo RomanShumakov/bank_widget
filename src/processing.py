72f4b23e4ded68eb29f0453f5ecb6046a3afb18e
Роман Шумаков <romeo46100@mail.ru>
2026-04-15 02:19:56 +0300

    add sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -5,3 +5,6 @@ def filter_by_state(data_list: list[dict], state='EXECUTED') -> list[dict]:
             result_list.append(diction)
     return result_list
 
+def sort_by_date(datatime_list: list[dict], turn_of_sort=True) -> list[dict]:
+    return sorted(datatime_list, key=lambda date: date["date"], reverse=turn_of_sort)
+
