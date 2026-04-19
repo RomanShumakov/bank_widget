43b50e50755b9123651f99041682aab8e18f81fb
Роман Шумаков <romeo46100@mail.ru>
2026-04-19 23:56:52 +0300

    done and complete 8 tests mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -1,16 +1,17 @@
 from src.masks import get_mask_account, get_mask_card_number
+from typing import Union
 
 
-def mask_account_card(card: str) -> str:
+def mask_account_card(card: Union[int, str]) -> str:
     """Функция обработки информации как о картах, так и о счетах"""
     card = str(card)
     splited_card = card.split()
     words_list = []
     for word in splited_card:
         if word.isdigit() and len(word) == 16:
-            word = get_mask_card_number(int(word))
+            word = get_mask_card_number(word)
         if word.isdigit() and len(word) == 20:
-            word = get_mask_account(int(word))
+            word = get_mask_account(word)
 
         words_list.append(word)
     masked_card = " ".join(words_list)
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,6 +1,11 @@
 from src.widget import mask_account_card, get_date
 import pytest
 
-@pytest.mark.parametrize("card_id, result", [(8765432112345678, "8765 43** **** 5678")])
+
+@pytest.mark.parametrize("card_id, result",
+                         [(8765432112345678, "8765 43** **** 5678"), ("8765432112345678", "8765 43** **** 5678"),
+                          ("87654321123456781111", "**1111"), ("123", "123"), (321, "321"),
+                          ("", ""), ("Master Card 1111222233334444", "Master Card 1111 22** **** 4444"),
+                          ("Счет 11111222223333344444", "Счет **4444")])
 def test_mask_account_card(card_id, result):
     assert mask_account_card(card_id) == result
