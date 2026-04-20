6893b133a195bd8e574308764d74c6dc236853ad
Роман Шумаков <romeo46100@mail.ru>
2026-04-20 15:42:45 +0300

    added docstrings for fixtures in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,16 +3,19 @@ import pytest
 
 @pytest.fixture
 def card_num():
+    """Фикстура для проверки номера карты в test_get_mask_card_number"""
     return 1234567887654321
 
 
 @pytest.fixture
 def card_id():
+    """Фикстура для проверки номера счета в get_mask_account"""
     return 12345678876543211234
 
 
 @pytest.fixture
 def list_of_dicts():
+    """Фикстура для проверки функций фильтрации и маскировки в processing.py"""
     return [
         {"name": "MOEX", "state": "CANCELED", "date": "2026-04-20-12-00-00"},
         {"name": "AAPL", "state": "CANCELED", "date": "2025-01-20-23-00-00"},
