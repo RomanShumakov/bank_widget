f610b7fb77ae0acf220024f1f49d77125cbadddd
Роман Шумаков <romeo46100@mail.ru>
2026-04-20 21:06:26 +0300

    update masks.py from Pull Requests

--- src/masks.py
+++ src/masks.py
@@ -1,21 +1,19 @@
 from typing import Union
 
 
-def get_mask_card_number(card_num: Union[int, str]) -> str:
+def get_mask_card_number(card_num: int | str) -> str:
     """Функция маскировки номера банковской карты"""
     card_id_str = str(card_num)
-    if len(card_id_str) == 16:
-        mask_card_number = card_id_str[:4] + " " + card_id_str[4:6] + "** **** " + card_id_str[-4:]
-        return mask_card_number
-    else:
+    if len(card_num) != 16:
         raise ValueError("Неправильно введен номер банковской карты")
 
+    return card_id_str[:4] + " " + card_id_str[4:6] + "** **** " + card_id_str[-4:]
 
-def get_mask_account(card_id: Union[int, str]) -> str:
+
+def get_mask_account(card_id: int | str) -> str:
     """Функция маскировки номера банковского счета"""
+
     card_id = str(card_id)
-    if len(card_id) == 20:
-        mask_account = "**" + card_id[-4:]
-        return mask_account
-    else:
+    if len(card_id) != 20:
         raise ValueError("Неправильно введен номер банковского счета")
+    return "**" + card_id[-4:]
