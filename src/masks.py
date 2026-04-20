652e4734b435bd16837e584b2be4cabf1f6dda6b
Роман Шумаков <romeo46100@mail.ru>
2026-04-20 21:44:58 +0300

    deleted int in typehints masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,19 +1,17 @@
 from typing import Union
 
 
-def get_mask_card_number(card_num: int | str) -> str:
+def get_mask_card_number(card_num: str) -> str:
     """Функция маскировки номера банковской карты"""
-    card_id_str = str(card_num)
     if len(card_num) != 16:
         raise ValueError("Неправильно введен номер банковской карты")
 
-    return card_id_str[:4] + " " + card_id_str[4:6] + "** **** " + card_id_str[-4:]
+    return card_num[:4] + " " + card_num[4:6] + "** **** " + card_num[-4:]
 
 
-def get_mask_account(card_id: int | str) -> str:
+def get_mask_account(card_id: str) -> str:
     """Функция маскировки номера банковского счета"""
 
-    card_id = str(card_id)
     if len(card_id) != 20:
         raise ValueError("Неправильно введен номер банковского счета")
     return "**" + card_id[-4:]
