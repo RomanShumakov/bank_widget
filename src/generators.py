dd00fc6c6e70b0ea658f5856c905c3abd0f99bcc
Роман Шумаков <romeo46100@mail.ru>
2026-05-01 23:51:04 +0300

    created card_number_generator in generators.py

--- src/generators.py
+++ src/generators.py
@@ -6,3 +6,14 @@ def filter_by_currency(transactions: list[dict], code: str = "USD"):
 def transaction_descriptions(transactions: list[dict], code: str = "USD"):
     for transaction in transactions:
         yield transaction["description"]
+
+def card_number_generator(start: int, stop: int):
+    begin = start
+    while begin <= stop:
+        result = "0000000000000000" + str(begin)
+
+        while len(result) > 16:
+            update_result = result[1:]
+            result = "" + update_result
+        yield result[:4] + " " + result[4:8] + " " + result[8:12] + " " +result[12:]
+        begin += 1
