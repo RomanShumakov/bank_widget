30b72246fb61b676a644dc286d9161eca5f3a77b
Роман Шумаков <romeo46100@mail.ru>
2026-05-01 23:03:44 +0300

    created transaction_descriptions in generators.py

--- src/generators.py
+++ src/generators.py
@@ -2,3 +2,7 @@ def filter_by_currency(transactions: list[dict], code: str = "USD"):
     for transaction in transactions:
         if transaction["operationAmount"]["currency"]["name"] == code:
             yield transaction
+
+def transaction_descriptions(transactions: list[dict], code: str = "USD"):
+    for transaction in transactions:
+        yield transaction["description"]
