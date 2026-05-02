41fbdcb31934f8a1d354dc826e498ac45e285dab
Роман Шумаков <romeo46100@mail.ru>
2026-05-02 15:46:30 +0300

    passed test for correct data in transaction_descriptions

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,5 +1,5 @@
-from src.generators import filter_by_currency
-#transaction_descriptions, card_number_generator
+from src.generators import filter_by_currency, transaction_descriptions
+#, card_number_generator)
 #import pytest
 
 
@@ -93,3 +93,10 @@ def test_filter_by_currency_empty():
     generator = filter_by_currency([{}, {}])
     assert next(generator) == {}
     assert next(generator) == {}
+
+def test_transaction_descriptions(dicts_list):
+    generator = transaction_descriptions(dicts_list)
+    assert next(generator) == "Перевод организации"
+    assert next(generator) == "Перевод со счета на счет"
+    assert next(generator) == "Перевод со счета на счет"
+    assert next(generator) == "Перевод с карты на карту"
