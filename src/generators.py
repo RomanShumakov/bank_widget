bc4f5096ef1889372dcdb2cabe8703a399e58597
Роман Шумаков <romeo46100@mail.ru>
2026-05-03 22:40:40 +0300

    in generators.py added docstrings, typehints, complete linkers, changed filter_by_currency for mypy

--- src/generators.py
+++ src/generators.py
@@ -1,19 +1,26 @@
-def filter_by_currency(transactions: list[dict], code: str = "USD"):
+from typing import Generator
+
+
+def filter_by_currency(transactions: list[dict], code: str = "USD") -> Generator[dict]:
+    """Функция возврата итератора, поочередно выдающего транзакции, где валюта операции соответствует заданной (например, USD)"""
     transaction_count = 0
     for transaction in transactions:
 
         if transaction == {}:
             transaction_count += 1
             yield {}
-        if transaction.get("operationAmount") and transaction.get("operationAmount").get(
-                "currency") and transaction.get("operationAmount").get("currency").get("code") == code:
-            transaction_count += 1
-            yield transaction
+        operation_amount = transaction.get("operationAmount")
+        if operation_amount:
+            currency = operation_amount.get("currency")
+            if currency and currency.get("code") == code:
+                transaction_count += 1
+                yield transaction
     if transaction_count == 0:
         yield {}
 
 
-def transaction_descriptions(transactions: list[dict]):
+def transaction_descriptions(transactions: list[dict]) -> Generator[dict]:
+    """Генераторная функция возврата описания каждой операции по очереди"""
     for transaction in transactions:
         if transaction == {}:
             yield {}
@@ -21,7 +28,8 @@ def transaction_descriptions(transactions: list[dict]):
             yield transaction.get("description", {})
 
 
-def card_number_generator(start: int, stop: int):
+def card_number_generator(start: int, stop: int) -> Generator[str]:
+    """Функция, генерирующая номера банковских карт  в формате XXXX XXXX XXXX XXXX"""
     begin = start
     while begin <= stop:
         result = "0000000000000000" + str(begin)
