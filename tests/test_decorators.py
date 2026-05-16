ffc73001cf664a519d2d2852cff0edca5f50eb80
Роман Шумаков <romeo46100@mail.ru>
2026-05-16 21:25:43 +0300

    done log tests for output into file, done 100% coverage

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,4 +1,6 @@
 import pytest
+import tempfile
+import os
 
 from src.decorators import log
 
@@ -6,19 +8,40 @@ from src.decorators import log
 def my_function():
     return "result"
 
-@log()
-def error_function():
-    raise ValueError("something went wrong...")
-
-
 def test_log_console_output(capsys):
     my_function()
     captured = capsys.readouterr()
     assert "Функция my_function успешно завершила выполнение за" in captured.out
     assert "result" in captured.out
 
+@log()
+def error_function():
+    raise ValueError("something went wrong...")
+
 def test_log_console_error(capsys):
     error_function()
     captured = capsys.readouterr()
     assert "error_function error: ValueError. Inputs: (), {}" in captured.out
     assert "Время выполнения:" in captured.out
+
+
+@log(filename="tempfile.txt")
+def test_function():
+    return "test result 45"
+
+def test_log_file_output():
+    test_function()
+    with open("tempfile.txt", "r", encoding='utf-8') as file:
+        content = file.read().encode('utf-8')
+        assert "test result 45" in str(content)
+
+
+@log(filename="tempfile.txt")
+def test_error_function():
+    raise ValueError("something went wrong...")
+
+def test_log_file_error():
+    test_error_function()
+    with open("tempfile.txt", "r", encoding='utf-8') as file:
+        content = file.read().encode('utf-8')
+        assert "test_error_function error:" in str(content)
