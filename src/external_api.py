bbb1aa5c7b4f7e7fc17e7a1448c5c569b547d337
Роман Шумаков <romeo46100@mail.ru>
2026-05-24 13:58:14 +0300

    done tests for transaction_in_rub

--- src/external_api.py
+++ src/external_api.py
@@ -5,8 +5,13 @@ import os
 def transaction_in_rub(transaction):
     load_dotenv()
     api_key = os.getenv("API_KEY")
-    currency = transaction.get("operationAmount").get("currency").get("code")
-    amount = transaction.get("operationAmount").get("amount")
+    if (type(transaction) != dict) or transaction == {}:
+        return "uncorrect income data"
+    if transaction.get("operationAmount") and transaction["operationAmount"].get("currency"):
+        currency = transaction["operationAmount"]["currency"].get("code", "uncorrect income data")
+    else:
+        currency = "uncorrect income data"
+    amount = transaction.get("operationAmount", {}).get("amount", "uncorrect income data")
     response = requests.get(f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}", headers={"apikey": api_key})
 
     total_rub_amount = response.json().get("result", "uncorrect payload data")
--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -52,3 +52,22 @@ def test_transaction_in_rub_uncorrect_payload_data(mock_get):
     }
     assert transaction_in_rub(oper) == "uncorrect payload data"
     mock_get.assert_called_once_with("https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount=8221.37", headers={"apikey": api_key})
+
+
+@patch('requests.get')
+def test_transaction_in_rub_uncorrect_income_data(mock_get):
+    mock_get.return_value.json.return_value = {'success': True,
+                                               'query': {'from': 'USD', 'to': 'RUB', 'amount': 8221.37},
+                                               'info': {'timestamp': 1779613387, 'rate': 71.790516},
+                                               'date': '2026-05-24', 'result': 590216.394527}
+
+    assert transaction_in_rub({}) == "uncorrect income data"
+
+@patch('requests.get')
+def test_transaction_in_rub_uncorrect_income_format(mock_get):
+    mock_get.return_value.json.return_value = {'success': True,
+                                               'query': {'from': 'USD', 'to': 'RUB', 'amount': 8221.37},
+                                               'info': {'timestamp': 1779613387, 'rate': 71.790516},
+                                               'date': '2026-05-24', 'result': 590216.394527}
+
+    assert transaction_in_rub([]) == "uncorrect income data"
