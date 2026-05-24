28c796ef32d1e11853b26ff7315a4c0241a5ad18
Роман Шумаков <romeo46100@mail.ru>
2026-05-24 12:20:47 +0300

    done transaction_in_rub with get server data

--- src/external_api.py
+++ src/external_api.py
@@ -8,29 +8,6 @@ def transaction_in_rub(transaction):
     currency = transaction["operationAmount"]["currency"]["code"]
     amount = transaction["operationAmount"]["amount"]
     response = requests.get(f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}", headers={"apikey": api_key})
-    return response
 
-
-    # rate = response.json()['rates']['RUB']
-    # amount = float(transaction['operationAmount']['amount']) * rate
-    # return amount
-
-# usd_trans = {
-#     "id": 41428829,
-#     "state": "EXECUTED",
-#     "date": "2019-07-03T18:35:29.512364",
-#     "operationAmount": {
-#       "amount": "8221.37",
-#       "currency": {
-#         "name": "USD",
-#         "code": "USD"
-#       }
-#     },
-#     "description": "Перевод организации",
-#     "from": "MasterCard 7158300734726758",
-#     "to": "Счет 35383033474447895560"
-#   }
-# res = transaction_in_rub(usd_trans)
-# print(res.text)
-# print("++++++++++++++")
-# print(res.json())
+    total_rub_amount = response.json()["result"]
+    return total_rub_amount
