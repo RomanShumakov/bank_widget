07b2087de6a035028a7477d268ae041a5ed326b1
Роман Шумаков <romeo46100@mail.ru>
2026-05-24 13:09:00 +0300

    fixed KeyError in dicts by .get

--- src/external_api.py
+++ src/external_api.py
@@ -5,8 +5,8 @@ import os
 def transaction_in_rub(transaction):
     load_dotenv()
     api_key = os.getenv("API_KEY")
-    currency = transaction["operationAmount"]["currency"]["code"]
-    amount = transaction["operationAmount"]["amount"]
+    currency = transaction.get("operationAmount").get("currency").get("code")
+    amount = transaction.get("operationAmount").get("amount")
     response = requests.get(f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}", headers={"apikey": api_key})
 
     total_rub_amount = response.json()["result"]
