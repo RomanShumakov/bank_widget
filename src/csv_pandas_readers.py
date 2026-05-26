565d633a103dfc32b2ec9d118c9e08183bf1817f
Роман Шумаков <romeo46100@mail.ru>
2026-05-27 01:02:42 +0300

    completed linkers for csv and pandas readers

--- src/csv_pandas_readers.py
+++ src/csv_pandas_readers.py
@@ -1,15 +1,21 @@
 import csv
-import pandas as pd
 import os
 
-def csv_reader(path_to_file:str = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data/transactions.csv"))) -> list[dict]:
+import pandas as pd
+
+
+def csv_reader(
+    path_to_file: str = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data/transactions.csv"))
+) -> list[dict]:
     """Функция для считывания финансовых операций из CSV"""
     with open(path_to_file) as file:
         reader = csv.DictReader(file)
     return list(reader)
 
-def excel_reader(path_to_file:str = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data/transactions_excel.xlsx"))) -> list[dict]:
+
+def excel_reader(
+    path_to_file: str = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data/transactions_excel.xlsx"))
+) -> list[dict]:
     """Функция для считывания финансовых операций из Excel"""
     excel_data = pd.read_excel(path_to_file)
-    return excel_data.to_dict(orient='records')
-
+    return excel_data.to_dict(orient="records")
