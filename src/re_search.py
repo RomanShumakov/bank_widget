a3b93cc84c106a45f17135d0f0b46ff8c2abe8f1
Роман Шумаков <romeo46100@mail.ru>
2026-05-30 01:20:46 +0300

    created process_bank_operations (count of categories))

--- src/re_search.py
+++ src/re_search.py
@@ -1,4 +1,5 @@
 import re
+from collections import Counter
 
 
 def process_bank_search(data:list[dict], search:str) -> list[dict]:
@@ -6,3 +7,9 @@ def process_bank_search(data:list[dict], search:str) -> list[dict]:
 
     pattern = re.compile(search)
     return [operation for operation in data if pattern.search(operation["description"])]
+
+
+def process_bank_operations(data:list[dict], categories:list)->dict:
+    """Функция возврата колличества операций по каждой указанной категории на основе списка банковских операций"""
+
+    return dict(Counter([operation["description"] for operation in data if operation["description"] in categories]))
